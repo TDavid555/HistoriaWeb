ed3f7a76ee1d766162fc09c303d36dcde81acf44
Kpatyi <116498778+Kpatyi@users.noreply.github.com>
2026-03-11 22:40:56 +0100

    frontend_verzio1.12

--- Frontend/HistoriaWeb/src/app/navbar/navbar.component.ts
+++ Frontend/HistoriaWeb/src/app/navbar/navbar.component.ts
@@ -9,12 +9,14 @@ import { BejelentkezesService } from '../bejelentkezes.service';
 })
 export class NavbarComponent implements OnInit{
   fiok:any=null;
+  id:string="";
   constructor(private bejelentkezesService:BejelentkezesService,private router:Router){}
 
   ngOnInit(): void {
     this.bejelentkezesService.Id.subscribe(()=>{
       this.bejelentkezesService.getFiokById().subscribe(fiok=>{
         this.fiok=fiok;
+        this.id=localStorage.getItem("id");
       });
     });
   }
@@ -22,7 +24,7 @@ export class NavbarComponent implements OnInit{
   Kijelentkezes(): void {
     localStorage.removeItem("id");
     this.bejelentkezesService.changeId();
-    this.fiok=null;
+    this.id=localStorage.getItem("id");
     this.router.navigate(["/hitelesito"]);
   }
 }
--- Frontend/HistoriaWeb/src/app/osszes-tortenet/osszes-tortenet.component.ts
+++ Frontend/HistoriaWeb/src/app/osszes-tortenet/osszes-tortenet.component.ts
@@ -21,75 +21,47 @@ export class OsszesTortenetComponent implements OnInit {
     private telepulesekService: TelepulesekService
   ) {}
 
-  ngOnInit(): void {
-    console.log('OsszesTortenetComponent inicializálva');
-    
-    this.telepulesekService.getMegyek().subscribe({
-      next: (megyek) => {
-        this.megyek = megyek;
-        console.log('Megyék betöltve:', this.megyek.length, 'db');
-        console.log('Megyék:', megyek);
-      },
-      error: (err) => {
-        console.error('Hiba a megyék betöltésekor:', err);
-      }
+  ngOnInit(): void {    
+    this.telepulesekService.getMegyek().subscribe(megyek=>{
+      this.megyek = megyek;
     });
 
-    this.tortenetekService.getOsszesTortenetek().subscribe({
-      next: (tortenetek) => {
-        console.log('Történetek betöltve:', tortenetek.length, 'db');
+    this.tortenetekService.getOsszesTortenetek().subscribe(tortenetek=>{
         this.osszes_tortenet = tortenetek.map(i => {
           i.keletkezes_datum = new Date(i.keletkezes_datum).toLocaleDateString();
           i.tortenet=i.tortenet.split(" ").slice(0,20).join(" ")
           return i;
         });
         this.tortenetek = [...this.osszes_tortenet];
-        console.log('Feldolgozott történetek:', this.tortenetek.length, 'db');
-      },
-      error: (err) => {
-        console.error('Hiba a történetek betöltésekor:', err);
-      }
     });
   }
 
   onMegyeChange(): void {
-    console.log('Megye választva:', this.valasztott_megye);
     this.telepulesek = [];
     this.valasztott_telepules = '';
     
     if (this.valasztott_megye) {
-      this.telepulesekService.getTelepulesekByMegye(this.valasztott_megye).subscribe({
-        next: (telepulesek) => {
+      this.telepulesekService.getTelepulesekByMegye(this.valasztott_megye).subscribe(telepulesek=>{
           this.telepulesek = telepulesek;
-          console.log('Települések betöltve:', this.telepulesek.length, 'db');
-        },
-        error: (err) => {
-          console.error('Hiba a települések betöltésekor:', err);
-        }
       });
     }
   }
 
-  kereses(): void {
-    console.log('Keresés indítva. Választott település:', this.valasztott_telepules);
-    
+  kereses(): void {    
     if (this.valasztott_telepules) {
       this.tortenetek = this.osszes_tortenet.filter(t => 
         t.telepules === this.valasztott_telepules
       );
-      console.log('Szűrt történetek:', this.tortenetek.length, 'db');
-    } else {
+    } 
+    else {
       this.tortenetek = [...this.osszes_tortenet];
-      console.log('Összes történet megjelenítve:', this.tortenetek.length, 'db');
     }
   }
 
   szurestTorlese(): void {
-    console.log('Szűrés törlése');
     this.valasztott_megye = '';
     this.valasztott_telepules = '';
     this.telepulesek = [];
     this.tortenetek = [...this.osszes_tortenet];
-    console.log('Szűrés törölve, összes történet:', this.tortenetek.length, 'db');
   }
 }
